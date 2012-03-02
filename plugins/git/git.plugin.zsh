de0d574661ebe201b55974db6282813d0cf0d37f
seenmyfate <seenmyfate@gmail.com>
2012-03-02 12:13:52 +0000

    changing some git short cuts

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -3,30 +3,20 @@ alias g='git'
 compdef g=git
 alias gs='git status'
 compdef _git gst=git-status
-alias gl='git pull'
-compdef _git gl=git-pull
+alias gl='git log --oneline --graph'
+compdef _git gl=git-log
 alias gf='git fetch'
 compdef _git gf=git-fetch
-alias gup='git fetch && git rebase'
-compdef _git gup=git-fetch-rebase
-alias gp='git push'
-compdef _git gp=git-push
-gdv() { git-diff -w "$@" | view - }
-compdef _git gdv=git-diff
+alias gfo='git fetch origin'
+compdef _git gfo=git-fetch-origin
 alias gc='git commit -v'
 compdef _git gc=git-commit
-alias gca='git commit -v -a'
-compdef _git gca=git-commit
 alias gco='git checkout'
 compdef _git gco=git-checkout
-alias gb='git branch'
-compdef _git gb=git-branch
-alias gba='git branch -a'
-compdef _git gba=git-branch
+alias gb='git checkout -b'
+compdef _git gb=git-checkout-b
 alias gcount='git shortlog -sn'
 compdef gcount=git
-alias gcp='git cherry-pick'
-compdef _git gcp=git-cherry-pick
 alias glg='git log --stat --max-count=5'
 compdef _git glg=git-log
 alias glgg='git log --graph --max-count=5'
